a2e5d773bd5d82ad0c3382ad8f631268053b2c04
madblobfish <madblobfish@users.noreply.github.com>
2017-10-30 15:47:58 +0100

    Array.count_unique more beautiful code

--- ruby/stdlib/array/count_unique.rb
+++ ruby/stdlib/array/count_unique.rb
@@ -1,5 +1,7 @@
 class Array
   def count_unique
-    self.each_with_object(Hash.new(0)){|i,h| h[i] += 1}
+    self.group_by(&:itself).transform_values(&:size)
+    # older rubies
+    # self.each_with_object(Hash.new(0)){|i,h| h[i] += 1}
   end
 end
