07456bc84ffc4dc1e5e8350a7f90364bec6d9fef
madblobfish <madblobfish@users.noreply.github.com>
2018-08-11 09:53:35 +0200

    ruby: ipaddr: extended to_s function with binary output ip.to_s(2)

---
+++ ruby/stdlib/ipaddr/to_s.rb
@@ -0,0 +1,18 @@
+class IPAddr
+  alias :to_s_orig :to_s
+  def to_s(b=nil)
+    if b == 2
+      if self.ipv4?
+        ('%032b' % self.to_i).split("").each_slice(8).map(&:join).join(".")
+      elsif self.ipv6?
+        raise ArgumentError, 'AHHHHHH, not implemented! yet'
+      else
+        raise ArgumentError, 'AHHHHHH'
+      end
+    elsif ! b.nil?
+      raise ArgumentError, 'only 2 or nil accepted'
+    else
+      to_s_orig
+    end
+  end
+end
