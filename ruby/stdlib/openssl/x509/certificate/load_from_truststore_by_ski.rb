45c9733032f298f399bf3b62433aace1ebd12917
madblobfish <madblobfish@users.noreply.github.com>
2019-04-10 08:59:09 +0200

    use a more sane default

--- ruby/stdlib/openssl/x509/certificate/load_from_truststore_by_ski.rb
+++ ruby/stdlib/openssl/x509/certificate/load_from_truststore_by_ski.rb
@@ -5,7 +5,7 @@ module OpenSSL
   module X509
     class Certificate
       # certfolder could be e.g. from https://github.com/nabla-c0d3/trust_stores_observatory
-      def self.load_from_truststore_by_ski(ski, certfolder = "#{ENV['HOME']}/dev/opensource/trust_stores_observatory/certificates/")
+      def self.load_from_truststore_by_ski(ski, certfolder = "/etc/ssl/certs/")
         trusts = Dir[certfolder + "/*"]
           .map{|f| OpenSSL::X509::Certificate.new(File.read(f)) rescue nil}.compact
           .group_by{|c|c.subjectKeyIdentifier}[ski]&.first
