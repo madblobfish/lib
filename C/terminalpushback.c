f3e7951c7ad211c28af1208fd8a40c1748851084
madblobfish <madblobfish@users.noreply.github.com>
2019-09-20 12:46:32 +0200

    terminalpushback experiment

---
+++ C/terminalpushback.c
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <string.h>
+
+static int ioctl64(int fd, unsigned long nr, void *arg) {
+	errno = 0;
+	return syscall(__NR_ioctl, fd, nr, arg);
+}
+
+int main(void) {
+	char* pushback = "echo hi\n";
+
+	for(int i = 0; i < strlen(pushback); ++i){
+		ioctl64(0, TIOCSTI, &pushback[i]);
+	}
+}
