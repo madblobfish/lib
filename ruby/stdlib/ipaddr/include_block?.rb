3759de8155f6c8f819ab0e52d6ba5f09a3c3ee0d
madblobfish <madblobfish@users.noreply.github.com>
2019-10-07 16:03:13 +0200

    ruby: ipaddr: include_block? added check for the class

--- ruby/stdlib/ipaddr/include_block?.rb
+++ ruby/stdlib/ipaddr/include_block?.rb
@@ -1,5 +1,6 @@
 class IPAddr
   def include_block?(other)
+    raise ArgumentError, "other is not a IPAddr" unless other.is_a?(IPAddr)
     self.to_range.first <= other.to_range.first &&
     self.to_range.last  >= other.to_range.last
   end
