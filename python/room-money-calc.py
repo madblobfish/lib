268c4f2fb01b958de4c8a9aa8ee2f5fc6cd4d505
madblobfish <madblobfish@users.noreply.github.com>
2019-12-30 20:05:56 +0100

    python: minimal fixes for the money room calculator tool we all like so much

--- python/room-money-calc.py
+++ python/room-money-calc.py
@@ -1,8 +1,8 @@
 EURO_BILL_SIZE = {
-	5: 120*62,
-	10: 127*67,
-	20: 133*72,
-	50: 140*77,
+	  5: 120*62,
+	 10: 127*67,
+	 20: 133*72,
+	 50: 140*77,
 	100: 147*82,
 	200: 153*82,
 	500: 160*82,
@@ -16,7 +16,6 @@ def how_much_money_fits_mah_room_all_bills(room_area, room_height=2.35):
 
 import sys
 if __file__ == sys.argv[0]:
-
 	if len(sys.argv) == 4:
 		print(how_much_money_fits_mah_room(int(sys.argv[3]), float(sys.argv[1]), float(sys.argv[2])))
 	elif len(sys.argv) == 3:
@@ -26,5 +25,6 @@ if __file__ == sys.argv[0]:
 	else:
 		print(__file__ + " <room_area in m²> [<room_height in m default 2.35> [<euro_bill default all>]]\n")
 		print("This program calculates the amount of money you could put into your room.")
-		exit(1)
+		if len(sys.argv) != 1:
+			exit(1)
 	exit(0)
