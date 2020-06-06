41dabe8524bec6f3490fe5652938296ec42e739c
madblobfish <madblobfish@users.noreply.github.com>
2020-06-06 19:00:06 +0200

    ruby: games: tetris further implementation

--- ruby/games/tetris.rb
+++ ruby/games/tetris.rb
@@ -1,3 +1,14 @@
+require_relative 'lib/gamelib.rb'
+
+class Array
+  def rotate_clock
+    self.transpose.map(&:reverse)
+  end
+  def at
+  end
+end
+
+# Tetrinal
 class Tetris < TerminalGame
   EMPTY_CELL = {symbol:' ', color: [255,255,255]}
   def initialize(size_x=9, size_y=20)
@@ -12,12 +23,12 @@ class Tetris < TerminalGame
     color = (9..14).to_a.sample
     elem = [
       '####',
-      "###\n#",
+      "###\n#  ",
       "###\n  #",
-      "###\n #",
+      "###\n # ",
       "##\n##",
-      " #\n##\n#",
-      "#\n##\n #",
+      " #\n##\n# ",
+      "# \n##\n #",
     ].sample.split("\n").map{|e| e.split('').map{|e|
       case e
       when ' '
@@ -26,12 +37,23 @@ class Tetris < TerminalGame
         {symbol:'#', color: color}
       end
     }}
-    p elem
-    (0..3).to_a.sample.times{elem = elem.transpose.map(&:reverse)}
+    (0..3).to_a.sample.times{elem = elem.rotate_clock}
+    elem
   end
+
+  def drop_current_tile
+
+  end
+
   def draw(step=true)
     move_cursor()
-    print @board.map{|e| e.map{|e| "#{get_color_code(e[:color])}#{e[:symbol]}#{get_color_code()}" }.join }.join("\r\n")
+    clear()
+    drop_current_tile if step
+    print @board.each_with_index.map{|e, y| e.each_with_index.map do |e, x|
+      tile = "#{get_color_code(e[:color])}#{e[:symbol]}#{get_color_code()}"
+      droping_tile = @tile_current.fetch(x - @tile_current_pos.first, nil)&.fetch(y - @tile_current_pos.last, nil)
+      tile = "#{get_color_code(droping_tile[:color])}#{droping_tile[:symbol]}#{get_color_code()}" if droping_tile
+    end.join }.join("\r\n")
     print "\r", '-' * @board.first.length
   end
   def input_handler(input)
@@ -43,6 +65,17 @@ class Tetris < TerminalGame
       " "    => :drop,   #space
     }[input]
     return if move.nil?
+    case move
+    when :rotate
+      @tile_current = @tile_current.rotate_clock
+    when :down
+      drop_current_tile
+    when :left
+      @tile_current_pos[0] -= 1
+    when :right
+      @tile_current_pos[0] += 1
+    when :drop
+    end
     draw(false)
   end
 end
