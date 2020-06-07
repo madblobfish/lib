e304c2d806c7b5efdde85d18399b4cb1e4fe2161
madblobfish <madblobfish@users.noreply.github.com>
2020-06-07 20:24:59 +0200

    ruby: games: tetris further implementation

--- ruby/games/tetris.rb
+++ ruby/games/tetris.rb
@@ -1,60 +1,91 @@
 require_relative 'lib/gamelib.rb'
 
+# theese are my dirty little secrets:
 class Array
   def rotate_clock
     self.transpose.map(&:reverse)
   end
-  def at
+  def add(other)
+    [self, other].transpose.map(&:sum)
+  end
+  def sub(other)
+    [self, other].transpose.map{|x|x.reduce(:-)}
   end
 end
+class Hash
+  def rotate!
+    sx = self[:size][1]
+    self.transform_keys!{|y,x| x.nil? ? y : [sx-x, y]}
+    self[:size].reverse!
+  end
+end
+
 
 # Tetrinal
 class Tetris < TerminalGame
   EMPTY_CELL = {symbol:' ', color: [255,255,255]}
   def initialize(size_x=9, size_y=20)
-    @board = Array.new(size_y){Array.new(size_x){EMPTY_CELL.dup}}
-    @fps = 1
+    @board = Hash.new
+    @size = [size_y, size_x]
+    @fps = 5
     @tile_current = gen_tile
-    @tile_current_pos = [0, 0]
     @tile_next = gen_tile
   end
 
   def gen_tile
-    color = (9..14).to_a.sample
-    elem = [
-      '####',
-      "###\n#  ",
-      "###\n  #",
-      "###\n # ",
+    color = rand(9..14)
+    elem = {}
+    [
+      "    \n    \n####    \n    ",
+      "   \n###\n#  ",
+      "   \n###\n  #",
+      "   \n###\n # ",
       "##\n##",
-      " #\n##\n# ",
-      "# \n##\n #",
-    ].sample.split("\n").map{|e| e.split('').map{|e|
-      case e
-      when ' '
-        EMPTY_CELL.dup
-      when '#'
-        {symbol:'#', color: color}
-      end
-    }}
-    (0..3).to_a.sample.times{elem = elem.rotate_clock}
+      "  #\n ##\n # ",
+      " # \n ##\n  #",
+    ].sample.split("\n").each_with_index{|s,i| s.split('').each_with_index{|s,j| elem[[i,j]] = {symbol:'#', color: color} if s == '#'; elem[:size] = [i,j]}}
+    rand(0..3).times{elem.rotate!}
+    elem[:pos] = [0,0]
     elem
   end
 
-  def drop_current_tile
+  def drop_current_tile(completely=false)
+    begin
+      @tile_current[:pos][0] += 1
+      if @board.any?{|k,v| @tile_current[k.sub(@tile_current[:pos])] rescue false}
+        @tile_current[:pos][0] -= 1
+        next_tile
+        return
+      elsif @tile_current.any?{|k,v| k.is_a?(Array) ? (k.first + @tile_current[:pos][0]+1) >= @size[0] : false}
+        @tile_current[:pos][0] -= 1
+        next_tile
+        return
+      end
+    end while completely
+  end
 
+  def next_tile
+    @tile_current.each{|k,v| @board[k.add(@tile_current[:pos])] = v if k.is_a?(Array)}
+    @tile_current = @tile_next
+    @tile_next = gen_tile
+    @board.group_by{|k,v| k.first}.select{|k,v| v.count == @size[1]}.each do |k,v|
+      v.map{|x|@board.delete(x.first)}
+    end
+    raise "you lost" if @board.any?{|(y,x),v| y <= 0 }
   end
 
   def draw(step=true)
     move_cursor()
     clear()
     drop_current_tile if step
-    print @board.each_with_index.map{|e, y| e.each_with_index.map do |e, x|
+    print @size[0].times.map{|y| @size[1].times.map do |x|
+      e = @board[[y,x]] || EMPTY_CELL
       tile = "#{get_color_code(e[:color])}#{e[:symbol]}#{get_color_code()}"
-      droping_tile = @tile_current.fetch(x - @tile_current_pos.first, nil)&.fetch(y - @tile_current_pos.last, nil)
+      droping_tile = @tile_current[[y,x].sub(@tile_current[:pos])]
       tile = "#{get_color_code(droping_tile[:color])}#{droping_tile[:symbol]}#{get_color_code()}" if droping_tile
+      tile
     end.join }.join("\r\n")
-    print "\r", '-' * @board.first.length
+    print "\r", '-' * @size[1]
   end
   def input_handler(input)
     move = {
@@ -67,14 +98,15 @@ class Tetris < TerminalGame
     return if move.nil?
     case move
     when :rotate
-      @tile_current = @tile_current.rotate_clock
+      @tile_current.rotate!
     when :down
       drop_current_tile
     when :left
-      @tile_current_pos[0] -= 1
+      @tile_current[:pos][1] -= 1
     when :right
-      @tile_current_pos[0] += 1
+      @tile_current[:pos][1] += 1
     when :drop
+      drop_current_tile(true)
     end
     draw(false)
   end
