920e81debe84fbbacb462fa8979df012d4361c27
- <->
2020-12-15 23:21:49 +0100

    ruby: stdlib: udp_port_puncher: removed protocol change

--- ruby/stdlib/udp_port_puncher.rb
+++ ruby/stdlib/udp_port_puncher.rb
@@ -7,7 +7,6 @@ Socket.udp_server_loop(1235){|msg, msg_src|
     msg_src.reply(STORAGE[msg].first.map(&:to_s).join("\t"))
     STORAGE[msg].last.reply(
       [
-        "0",
         msg_src.remote_address.ip_address,
         msg_src.remote_address.ip_port
       ].map(&:to_s).join("\t")
