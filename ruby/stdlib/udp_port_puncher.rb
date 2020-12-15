30c3338c848a2e40c061704134414dd3fa7ef812
- <->
2020-12-16 00:58:41 +0100

    ruby: stdlib: udp_port_puncher_client: finished

--- ruby/stdlib/udp_port_puncher.rb
+++ ruby/stdlib/udp_port_puncher.rb
@@ -5,13 +5,16 @@ STORAGE = {}
 Socket.udp_server_loop(1235){|msg, msg_src|
   if STORAGE.has_key? msg
     msg_src.reply(STORAGE[msg].first.map(&:to_s).join("\t"))
-    STORAGE[msg].last.reply(
+    other = STORAGE[msg].last
+    other.reply(
       [
         msg_src.remote_address.ip_address,
         msg_src.remote_address.ip_port
       ].map(&:to_s).join("\t")
     )
     STORAGE.delete(msg)
+#    other.close()
+#    msg_src.close()
   else
     STORAGE[msg] = WeakRef.new([[
       Time.now.to_i, msg_src.remote_address.ip_address,
