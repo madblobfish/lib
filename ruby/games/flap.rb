00ee442997402296fde75b0c745dc6ae1c7eec81
madblobfish <madblobfish@users.noreply.github.com>
2021-01-09 06:20:40 +0100

    ruby: games: flap: further implementation, next up are hitboxes for the pipes and some additions to the background

--- ruby/games/flap.rb
+++ ruby/games/flap.rb
@@ -1,57 +1,61 @@
 require_relative 'lib/gamelib.rb'
 
 class Bla < TerminalGame
+  BG_HEIGHT = 576
   def initialize
-    update_size(true)
+    @timer = 0
+    @movespeed = 3
     @offset = 35
     @fps = 20
-    @once = true
-    @pos = [1,0]
+    @bird = {pos: [50, BG_HEIGHT/2], yspeed: -5, imgs: []}
+    @objects = []
+    @spawnrate = 70
+  end
+  def initial_draw
+    @bg_img_id = kitty_graphics_img_load(File.read('./gfx/bg.png'))
+    @pipe = kitty_graphics_img_load(File.read('./gfx/pipe.png'))
+    @bird[:imgs] = 3.times.map{|x|kitty_graphics_img_load(File.read("./gfx/b#{x+1}.png"))}
+    draw
   end
   def draw
-    @bg_img_id ||= kitty_graphics_img_load(File.read('./gfx/bg.png'))
-    @pipe ||= kitty_graphics_img_load(File.read('./gfx/pipe.png'))
-    # pixel_per_cell_y = @size_y.to_f / @rows.to_i
     pixel_per_cell_x = @size_x.to_f / @cols.to_i
     move_cursor(0,0)
-    print "hallo welt!"
+    print "hallo welt! #{@timer/@fps}"
+    # bg
     kitty_graphics_img_clear(:by_z, -2)
-    # (@size_x.to_i/(576/2) + 1).times do |x|
-    #   pos_x = (@offset/pixel_per_cell_x).floor + ((576/2)/pixel_per_cell_x.floor).floor*x
-    #   move_cursor(x, [pos_x,0].max)
-    #   # if pos_x == 0
-    #   #   cell_x = 0
-    #   #   img_x = 576 - @offset
-    #   # else
-    #     cell_x = (@offset%pixel_per_cell_x).floor
-    #     img_x = 576/2
-    #   # end
-    #   kitty_graphics_img_display(@bg_img_id, z: -2, cell_x: cell_x, img_x: img_x, width: 576/2) #, columns:@cols, rows:@rows)
-    #   # kitty_graphics_img_display(@bg_img_id, z: -2, cell_x: (@offset%pixel_per_cell_x).floor, img_x: 576/2, width: 576/2) #, columns:@cols, rows:@rows)
-    # end
-    (@size_x.to_i/(576/2) + 1).times do |x|
-      kitty_graphics_img_pixel_place(@bg_img_id, @offset + (576/2)*x-1, x*5, z: -2)
+    (@size_x.to_i/(BG_HEIGHT/2) + 1).times do |x|
+      kitty_graphics_img_pixel_place(@bg_img_id, @offset + (BG_HEIGHT/2)*x-1, 0, z: -2, width: BG_HEIGHT/2)
     end
-    move_cursor(3,0)
-    kitty_graphics_img_display(@bg_img_id, z: -2, img_x: 576 - @offset, width: 576/2)
-    # if @once
-    #   @once = false
-      # kitty_graphics_img_clear(:by_z, -1)
-      # kitty_graphics_img_pixel_place(@pipe, *@pos.reverse, z: -1)
-    # end
-    @offset -= 2
-    @offset = 576/2 if @offset <= 0
+    move_cursor(0,0)
+    kitty_graphics_img_display(@bg_img_id, z: -2, img_x: BG_HEIGHT/2 - @offset, width: BG_HEIGHT/2*0 +@offset)
+    # pipes
+    if @timer % @spawnrate == 0
+      @objects << {img_id: @pipe, x: @size_x.to_i, y: rand(200..400), background: true}
+      @objects << {img_id: @pipe, x: @size_x.to_i, y: @objects.last[:y] - 100, background: true}
+    end
+    @objects.map{|x|x[:img_id]}.uniq.each{|id| kitty_graphics_img_clear(:id, id)}
+    @objects.each do |obj|
+      kitty_graphics_img_pixel_place(obj[:img_id], obj[:x], obj[:y], width: obj.fetch(:width,0), height: obj.fetch(:height,0), z: obj.fetch(:z, 0))
+      obj[:x] -= @movespeed if obj.has_key?(:background)
+    end
+    @objects.reject!{|obj| obj[:x] + obj.fetch(:width, 1000) <= 0}
+    # bird
+    kitty_graphics_img_clear(:id, @bird[:imgs].first)
+    @bird[:imgs].rotate! if @timer % 4 == 0
+    kitty_graphics_img_pixel_place(@bird[:imgs].first, *@bird[:pos], z: -1)
+
+    raise "YU DED" if @bird[:pos][1] > 500 || @bird[:pos][1] < 0
+    @bird[:yspeed] = [@bird[:yspeed] + 2.6, 27].min
+    @bird[:pos][1] += @bird[:yspeed]
+
+    @timer += 1
+    @offset -= @movespeed
+    @offset = BG_HEIGHT/2 if @offset <= 0
   end
   def input_handler(input)
     case input
-    when "\e[A" # up
-      @pos[0] -= 1
-    when "\e[B" # down
-      @pos[0] += 1
-    when "\e[D" # left
-      @pos[1] -= 1
-    when "\e[C" # right
-      @pos[1] += 1
+    when "\e[A", " " # up
+      @bird[:yspeed] = -19
     end
   end
 end
