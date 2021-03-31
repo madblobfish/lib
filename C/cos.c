c66f48db1c21014ddb9b3bad26e6d2db813e30b0
- <->
2021-03-31 18:27:08 +0200

    C: cos: more x86 functions

--- C/cos.c
+++ C/cos.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double coss(double __X){
+// __attribute__((always_inline))
+/*inline*/ double _cos(double __X){
 	__asm__(
 		"fcos"
+		: "=t" (__X)
+		: "0" (__X)
+	);
+	return __X;
+}
+
+// __attribute__((always_inline))
+/*inline*/ double _sin(double __X){
+	__asm__(
+		"fsin"
+		: "=t" (__X)
+		: "0" (__X)
+	);
+	return __X;
+}
+
+// __attribute__((always_inline))
+/*inline*/ double _sincos_c(double __X){
+	double Y;
+	__asm__(
+		"fsincos"
+		: "=t" (__X), "=u" (Y)
+		: "0" (__X)
+	);
+	return __X;
+}
+// __attribute__((always_inline))
+/*inline*/ double _sincos_s(double __X){
+	double Y;
+	__asm__(
+		"fsincos"
+		: "=t" (Y), "=u" (__X)
+		: "0" (__X)
+	);
+	return __X;
+}
+
+// __attribute__((always_inline))
+/*inline*/ double _sqrt(double __X){
+	__asm__(
+		"fsqrt"
 		: "=t" (__X)
 		: "0" (__X)
 	);
@@ -14,7 +56,12 @@ int main(int argc, char const *argv[]){
 	if(argc == 2){
 		double in = atof(argv[1]);
 		printf("in : %4.8f\n", in);
-		printf("out: %4.8f\n", coss(in));
+		printf("cos: %4.8f\n", _cos(in));
+		printf("sin: %4.8f\n", _sin(in));
+		printf("sqrt: %4.8f\n", _sqrt(in));
+		printf("sincos_c: %4.8f\n", _sincos_c(in));
+		printf("sincos_s: %4.8f\n", _sincos_s(in));
+		printf("in : %4.8f\n", in);
 	}else{
 		puts("give me one float input!");
 		if(argc != 1){return 1;}
