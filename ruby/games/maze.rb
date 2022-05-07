6a579110ade8e30b42604cc96282108406a16860
- <->
2022-05-07 07:31:15 +0200

    ruby: games: maze: make harder and add some cli options and highscores (all in death mode)

--- ruby/games/maze.rb
+++ ruby/games/maze.rb
@@ -2,18 +2,24 @@ require_relative 'lib/gamelib'
 require_relative '../stdlib/color_palette.rb' rescue nil
 
 class Maze < TerminalGame
-  def initialize(size_x=70, size_y=16)
+  def initialize(size_x=70, size_y=16, death=false)
     @fps = :manual
     @size = [size_y, size_x]
     # @player = @size.map{|x|rand(x)}
     @player = @size.dup
     @path = [@player.dup]
-    @goal = [0,0]
+    # @goal = [0,0]
     # @goal = @size.map{|x|rand(x)}
     # @goal = @size.map{|x|rand(x)} if @goal == @player
     @map = maze_gen_bt(@size)
+    @goal = @map[:start]
+    @start_time = Time.now
     @backwalk = 0
+    @miswalk = 0
+    @steps = 0
+    @death = death
   end
+
   def maze_gen_bt(size=[70,16])
     GC.disable # more speed
     door_open_ud = Hash.new{false}
@@ -21,9 +27,9 @@ class Maze < TerminalGame
     walked = Hash.new{false}
     free_fields = (size[0]+1) * (size[1]+1)
 
-    path = [size.map{|x|rand(x)}]
+    path = [size.map{|x|rand((x+1)/2)}]
     start = path[0].dup
-    walked[path[0].dup] = true
+    walked[path[0].dup] = false
 
     while free_fields > 0 && path.any?
       pos = path.last
@@ -31,7 +37,7 @@ class Maze < TerminalGame
         next_pos = [pos[0]+x, pos[1]+y]
         next_pos.any?(&:negative?) ||
           next_pos.zip(size).any?{|a,b|a>b} ||
-          walked[next_pos] == true
+          walked.has_key?(next_pos)
       end
       if moves.any?
         move = moves.sample
@@ -43,7 +49,7 @@ class Maze < TerminalGame
         else
           door_open_ud[[pos, new_pos].min] = true
         end
-        walked[new_pos.dup] = true
+        walked[new_pos.dup] = false #path.length
         free_fields -=1
         path.push(new_pos.dup)
       else
@@ -54,15 +60,36 @@ class Maze < TerminalGame
       end
     end
     GC.enable
-    {ud:door_open_ud, lr:door_open_lr}
+    {ud:door_open_ud, lr:door_open_lr, start:start}
   end
 
+  def die(text='won')
+    raise <<~bla
+      #{text}
+      Miswalk: #{@miswalk}
+      Backwalk: #{@backwalk}
+      Steps: #{@steps}
+      Path: #{@path.length}
+      Time: #{Time.now - @start_time}s"
+    bla
+  end
   def check_win
-    raise "won #{@backwalk}" if @goal == @player
+    die if @goal == @player
+  end
+
+  def initial_draw
+    print get_color_code(15, :fg)
+    draw
   end
   def draw
     check_win()
     move_cursor
+    print ' Miswalk: ', @miswalk unless @death
+    print ' Backwalk: ', @backwalk
+    print ' Steps: ', @steps
+    print ' Path: ', @path.length
+    print ' Time: ', Time.now - @start_time
+    puts "s\r"
     puts '██' * (@size.first+2)
     print "\r"
     (@size.last*2+1).times.each do |y|
@@ -78,13 +105,13 @@ class Maze < TerminalGame
           if [x,y/2] == @goal
             print get_color_code(color_palette(0.5), :fg), '╬', get_color_code(15, :fg)
           elsif player_loc
-            print get_color_code(color_palette(1),:bg), 'X', get_color_code(0,:bg)
+            print get_color_code(color_palette(1),:bg), ' ', get_color_code(0,:bg)
           else
             print marker
           end
-          print @map[:lr][[x, y/2]] ? (player_loc ? ' ' : marker) : '█'
+          print @map[:lr][[x, y/2]] ? (@path.index([x+1,y/2]) ? marker : ' ') : '█'
         else
-          print @map[:ud][[x, y/2]] ? (player_loc ? ' ' : marker) : '█'
+          print @map[:ud][[x, y/2]] ? (@path.index([x,(y+1)/2]) ? marker : ' ') : '█'
           print '█'
         end
       end
@@ -101,6 +128,7 @@ class Maze < TerminalGame
       "\e[C" => [ 1,0],
     }[input]
     return if dir.nil?
+    @steps += 1
     old_pos = @player.dup
     @player[0] += dir[0]
     @player[1] += dir[1]
@@ -116,7 +144,8 @@ class Maze < TerminalGame
         @map[:ud][[@player, old_pos].min]
       end
     unless door_open
-      raise 'LOOOSER!'
+      die('LOOOSER!') if @death
+      @miswalk += 1
       @player = old_pos
       draw()
       return
@@ -126,4 +155,35 @@ class Maze < TerminalGame
   end
 end
 
-Maze.new(10,16).run()
+if __FILE__ == $PROGRAM_NAME
+  deth = ARGV.delete('--death') ? true : false
+  ARGV = ['eazy'] if ARGV.empty?
+  case ARGV.first
+  when 'eazy'
+    Maze.new(7,13,deth).run()
+    # Miswalk: 0
+    # Backwalk: 0
+    # Steps: 38
+    # Path: 39
+    # Time: 9.669698715s
+  when 'normal'
+    Maze.new(10,16,deth).run()
+    # Miswalk: 0
+    # Backwalk: 0
+    # Steps: 93
+    # Path: 84
+    # Time: 21.404004426s
+  when 'har'
+    Maze.new(15,40,deth).run()
+    # Miswalk: 0
+    # Backwalk: 39
+    # Steps: 255
+    # Path: 178
+    # Time: 88.105600061s
+  when 'hard'
+    Maze.new(28,100,deth).run()
+  else
+    puts 'hi we got 4 modi [hard|har|normal|eazy]'
+    puts 'try --death for more fun'
+  end
+end
