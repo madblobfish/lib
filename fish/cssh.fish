8d07e1d20166bb980ee2af456a35db723c9e29ec
- <->
2022-11-25 21:19:15 +0100

    fish: cssh: use kitty broadcast instead of send-text

--- fish/cssh.fish
+++ fish/cssh.fish
@@ -1,5 +1,5 @@
 function cssh
 	# cluster ssh implementation with kitty and fish
 	set -lx servers $argv
-	kitty --override allow_remote_control=yes fish -C 'for s in (string split " " $servers); kitty @ new-window --keep-focus --no-response --title ssh fish -C "set -x TERM xterm; ssh $s"; end; kitty @ goto-layout grid; echo --- Control all here --- ; kitty @ send-text --match title:ssh --stdin' &; disown
+	kitty --title 'cssh '$servers --override allow_remote_control=yes fish -c 'kitty @ goto-layout grid; for s in (string split " " $servers); kitty @ new-window --keep-focus --no-response --title ssh fish -C "set s $s; ssh $s"; end; kitty +kitten broadcast' &; disown
 end
