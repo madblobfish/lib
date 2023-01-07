cd1c1dd38138df2dc2846b71b19dfc46ffa8c607
- <->
2023-01-07 02:56:43 +0100

    ruby: games: malinder-db-pfusch: implement some merging strategies

--- ruby/games/malinder-db-pfusch.rb
+++ ruby/games/malinder-db-pfusch.rb
@@ -46,9 +46,59 @@ end
 YEAR_SEASON = {'winter'=>1, 'spring'=>2, 'summer'=>3, 'fall'=>4}
 csv.sort_by!{|e| x = e.values_at(1,2,0,4,5); x[1] = YEAR_SEASON[x[1]]; x.map(&:to_s).map(&:downcase)}
 csv.uniq!
-csv.map(&:first).compact.group_by{|e|e}.select{|k, v|v.size > 1}.each do |id, c|
-  STDERR.puts("Duplicates: #{c.count} of #{id}")
+csv.compact.group_by(&:first).select{|k, v|v.size > 1}.each do |id, c|
+  next if id == nil # ignore the nil group, you're on your own for now
+  c_stage_one = c.select{|c|c[3].start_with?(*%w(want nope okay))}
+  c_backlog = c.select{|c|c[3].start_with?(*%w(backlog))}
+  c_progress = c.select{|c|c[3].start_with?(*%w(partly paused))}
+  c_done = c.select{|c|c[3].start_with?(*%w(seen broken))}
+  if c.count == 2 && (c.first == c.last[..5] || c.first[..5] == c.last)
+    stays = c.max{|a,b| a.length <=> b.length}
+    STDERR.puts('removing lines due to being same, except end: ' + (c - stays).inspect)
+    STDERR.puts('stays: ' + stays.inspect)
+    csv -= c - stays
+  elsif c.reduce(true){|o,a| o && a.values_at(0,3) == c.first.values_at(0,3)}
+    if c.map{|a| a.values_at(*a.each_index.to_a - [1,2,4,5])}.uniq.one?
+      stays = c.sort.first
+      remainder = c - [stays]
+      STDERR.puts('removing lines due to same stuff: ' + remainder.inspect)
+      STDERR.puts('stays: ' + stays.inspect)
+      csv -= remainder
+    else
+      p c
+      raise 'all are same?'
+    end
+  elsif [c_stage_one,c_backlog,c_progress,c_done].map(&:any?).count(true) > 1
+    stays = if c_done.any?
+        c_done
+      elsif c_progress.any?
+        c_progress
+      elsif c_backlog.any?
+        c_backlog
+      end
+    STDERR.puts('removing lines due to more progressed stuff: ' + (c - stays).inspect)
+    STDERR.puts('stays: ' + stays.inspect)
+    csv -= c - stays
+  elsif c.map{|e| e[3]}.all?{|s| s.start_with?('broken')}
+    p c
+    raise 'all are broken'
+  elsif c.map{|e| e[3]}.all?{|s| s.start_with?('partly')}
+    latest =  c.max{|a,b| a[3].split(',').last.to_i <=> b[3].split(',').last.to_i}
+    remainder = c - [latest]
+    STDERR.puts('removing lines due to newer: ' + remainder.inspect)
+    STDERR.puts('stays: ' + latest.inspect)
+    csv -= remainder
+  elsif c.map{|e| e[3]}.all?{|s| s.start_with?('seen')}
+    p c
+    raise 'all are seen'
+  else
+    next if [41611, ].include?(id)
+    p c
+    raise 'couldn\'t merge'
+    STDERR.puts("Duplicates: #{c.count} of #{id}")
+  end
 end
+# raise 'stop hammertime' # to test the merging
 unless json
   puts CSV.generate(col_sep: "\t"){|o| csv.each{|r| o << r}}
 else
