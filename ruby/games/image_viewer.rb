41e0646af046f716ec0e32d1c37003438c24c029
- <->
2023-01-08 07:31:21 +0100

    ruby: games: image_viewer: implement some zooming, still broken

--- ruby/games/image_viewer.rb
+++ ruby/games/image_viewer.rb
@@ -7,6 +7,9 @@ class ImageViewer < TerminalGame
     @require_kitty_graphics = true
     @draw_status_line = agrgs.delete('--no-status').nil? ? true : false
     @random = agrgs.delete('--random')
+    @scale_up = :auto if agrgs.delete('--scale-up')
+    @zoom = 1
+    @zoom_pos = [0,0]
     if agrgs.delete('--rotate')
       @images_cycle = -1
       @fps = 1.0/5
@@ -55,26 +58,60 @@ class ImageViewer < TerminalGame
       @images_cycle += 1 if @rotate and cycle
       @images_cycle %= @images.size if @rotate and cycle
     end
-    current_filename, current_img = @images[@images_cycle]
+    current_filename, current_img, gif_params = @images[@images_cycle]
     rowsize = @draw_status_line ? @size_row : 0
-    scale_by = current_img.size.zip([@size_x, @size_y-rowsize]).map{|want,have| want > have ? have/want.to_f : 1}.min
-    buffer = (scale_by == 1 ? current_img : current_img.resize(scale_by)).pngsave_buffer
+    frames = []
+    if gif_params
+      frames = gif_params[0].times.map{|n| current_img.crop(0,gif_params[1]*n, current_img.size()[0], gif_params[1])}
+      current_img = frames.first
+    end
+    scale_by = current_img.size.zip([@size_x, @size_y-rowsize]).map do |want,have|
+      want > have ? have/want.to_f : (@scale_up == :auto ? have/want.to_f : 1)
+    end.min
+    if scale_by * @zoom != 1
+      current_img = current_img.resize(scale_by*@zoom) #, kernel: :nearest)
+      if @zoom != 1
+        size = current_img.size()
+        cutout_size = [@size_x, @size_y-rowsize].zip(size).map{|a,b|[[a,b].min,0].max}
+        @zoom_pos = @zoom_pos.zip(size, cutout_size).map{|a,b,c|[a,b-c].min}
+        current_img = current_img.crop(*@zoom_pos, *cutout_size)
+      end
+    end
+    buffer = current_img.pngsave_buffer
     clear
     if @draw_status_line
       move_cursor(@rows,0)
       f = current_filename.inspect
       f += " (#{@images_cycle}/#{@images.size})" if @images.size > 1
+      f += ' %.0f%%' % (scale_by*@zoom*100) if @images.size > 1
       f += ' Paused' if @roate_stopped
       print(' '*((@cols-f.length)/2)) rescue nil
       print(f)
     end
     move_cursor(0,0)
     imgid = kitty_graphics_img_load(buffer)
-    kitty_graphics_img_pixel_place_center(imgid, *current_img.size.map{|e| (e*scale_by).to_i}, 0, -rowsize)
+    kitty_graphics_img_pixel_place_center(imgid, *current_img.size.map{|e| (e).to_i}, 0, -rowsize)
+    # frames.each do |frame|
+    #   if scale_by * @zoom != 1
+    #     frame = frame.resize(scale_by*@zoom)
+    #     if @zoom != 1
+    #       size = frame.size()
+    #       cutout_size = [@size_x, @size_y-rowsize].zip(size).map{|a,b|[[a,b].min,0].max}
+    #       @zoom_pos = @zoom_pos.zip(size, cutout_size).map{|a,b,c|[a,b-c].min}
+    #       frame = frame.crop(*@zoom_pos, *cutout_size)
+    #     end
+    #   end
+    #   id = kitty_graphics_img_load(frame.pngsave_buffer)
+    #   kitty_graphics_img_pixel_place_center(id, *frame.size.map{|e| (e).to_i}, 0, -rowsize)
+    # end
   end
 
   def input_handler(input)
     case input
+    when "-"
+      @zoom /= 1.5
+    when "+"
+      @zoom *= 1.5
     when "q"
       exit()
     when " " #space
@@ -82,10 +119,25 @@ class ImageViewer < TerminalGame
     when "\e[C" #right
       @images_cycle += 1
       @images_cycle %= @images.size
+      @zoom_pos = [0,0]
+      @zoom = 1
     when "\e[D" #left
       @images_cycle -= 1
       @images_cycle = @images.size-1 if @images_cycle < 0
+      @zoom_pos = [0,0]
+      @zoom = 1
+    when "\e[1;5A" # ctrl up
+      @zoom_pos[1] -= @size_y/5
+      @zoom_pos[1] = 0 if @zoom_pos[1] < 0
+    when "\e[1;5B" # ctrl down
+      @zoom_pos[1] += @size_y/5
+    when "\e[1;5C" # ctrl right
+      @zoom_pos[0] += @size_x/5
+    when "\e[1;5D" # ctrl left
+      @zoom_pos[0] -= @size_x/5
+      @zoom_pos[0] = 0 if @zoom_pos[0] < 0
     else
+      # raise input.inspect # for implementing new commands
       return
     end
     sync_draw do
