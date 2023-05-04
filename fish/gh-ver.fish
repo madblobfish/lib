e8e2bf6d911583a8386200e5e93a9a5084db1710
- <->
2023-05-04 16:37:33 +0200

    fish: gh-ver: updated and simplified minimally

--- fish/gh-ver.fish
+++ fish/gh-ver.fish
@@ -1,4 +1,4 @@
 function gh-ver
 	# gets the latest version number from github
-	curl -s "https://github.com/"$argv"/releases/latest" | sed -re 's/^.+\/releases\/tag\/([^/]+)".+$/\1/'
+	curl -s "https://github.com/"$argv"/releases/latest" -I | sed -nre '/location/s/.*\///p'
 end
