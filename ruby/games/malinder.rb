1319a6b5f57a939b1e62b114497d25eeed8438ff
- <->
2023-06-04 02:00:23 +0200

    ruby: games: malinder: query - make timestamp and choice searchable and simplify fetch_values

--- ruby/games/malinder.rb
+++ ruby/games/malinder.rb
@@ -340,6 +340,8 @@ if __FILE__ == $PROGRAM_NAME
 		x = CACHE.map do |k, v|
 			if CHOICES.has_key?(k.to_s)
 				v['state'] = CHOICES[k.to_s][:choice].split(',').first
+				v['choice'] = CHOICES[k.to_s][:choice]
+				v['timestamp'] = CHOICES[k.to_s][:ts]
 			end
 			v.merge!(v['start_season'])
 			v['genres'] = v['genres']&.map{|h|h['name'].downcase}
@@ -347,8 +349,7 @@ if __FILE__ == $PROGRAM_NAME
 		end
 		date = Time.now.to_i
 		puts x.query(ARGV.join(' ')).map{|nime|
-			old = CHOICES.fetch(nime['id'].to_s, {})
-			nime.fetch_values('id', 'year', 'season') + [old.fetch(:choice, '-'), old.fetch(:ts, date)] + nime.fetch_values('title')
+			nime.fetch_values('id', 'year', 'season', 'choice', 'timestamp', 'title')
 		}.sort_by(&:first).map{|a| a.join("\t")}
 	elsif ARGV.first == 'stats'
 		load_all_to_cache
