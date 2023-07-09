f02f8b2c55db908b7a884aab450205c25042006b
- <->
2023-07-09 22:05:06 +0200

    ruby: terminalapps: malinder: fixed bug in writing to the logfile

--- ruby/terminalapps/malinder/malinder.rb
+++ ruby/terminalapps/malinder/malinder.rb
@@ -296,7 +296,7 @@ class MALinder < TerminalGame
 	def logchoice(choice)
 		anime = @season[@current]
 		return if CHOICES.has_key?(anime['id'].to_s)
-		which_season = anime['start_season'].fetch_values('season', 'year').to_s
+		which_season = anime['start_season'].fetch_values('season', 'year')
 		LOG_FILE.write("#{anime['id']}\t#{which_season.join("\t")}\t#{choice}\t#{Time.now.to_i}\t#{anime['title']}\n")
 		@season.delete_at(@current)
 		exit() if @season.empty?
