c354024e01d5abdf3d72f9ad60a9ed5d1f56753b
- <->
2023-07-09 21:40:14 +0200

    ruby: networkstuff: mac-lookup: replace File.dirname(__FILE__) with __dir__, slight reformatting

--- ruby/networkstuff/mac-lookup.rb
+++ ruby/networkstuff/mac-lookup.rb
@@ -1,4 +1,6 @@
 # download db from https://maclookup.app/downloads/csv-database
 require 'csv'
 raise 'gimme a mac or macprefix' unless ARGV.one?
-puts CSV.read(File.dirname(__FILE__) + '/.mac-vendors-export.csv', headers: true).find{|r| ARGV.first.upcase.start_with?(r["Mac Prefix"])}.to_h.map{|k,v|"#{k}: #{v}"}
+puts CSV.read(__dir__ + '/.mac-vendors-export.csv', headers: true).find{|r|
+	ARGV.first.upcase.start_with?(r["Mac Prefix"])
+}.to_h.map{|k,v|"#{k}: #{v}"}
