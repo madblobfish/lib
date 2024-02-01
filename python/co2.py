2a2fc6a7af53c0a79ac6bd421d4197a193aa5ab4
- <->
2024-02-01 04:15:51 +0100

    python: co2.py: minimal script to gather co2 sensor data

---
+++ python/co2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# original by: Henryk Plötz
+# https://hackaday.io/project/5301-reverse-engineering-a-low-cost-usb-co-monitor/log/17909-all-your-base-are-belong-to-us
+
+import sys, time
+
+with open(sys.argv[1], mode="b", buffering=0) as fp:
+    while True:
+        data = fp.read(8)
+        value = int.from_bytes(data[1:3])
+
+        match data[0]:
+            case 0x42:
+                print(f'co2sensor temp={value / 16.0 - 273.15}')
+            case 0x44 | 0x41:
+                print(f'co2sensor humid={value}')
+            case 0x50:
+                print(f'co2sensor co2={value}')
