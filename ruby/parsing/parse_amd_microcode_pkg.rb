4ffa1e28c738f770a4106bf817dc583fe9e2b1a5
- <->
2024-06-25 00:11:03 +0200

    ruby: parsing: parse_amd_microcode_pkg: some sanity checks and also handle output nicer

--- ruby/parsing/parse_amd_microcode_pkg.rb
+++ ruby/parsing/parse_amd_microcode_pkg.rb
@@ -3,6 +3,12 @@ def parse_amd_microcode_pkg(path)
   regexp = /(?<date>.\x20[\x01-\x31][\x01-\x13])(?<version>.{4})(?<loaderid>[\x00-\x04]\x80)(?<size>[\x00\x20\x10])(?<initflag>[\x00\x01])(?<checksum>.{4})(?<nb_vendor_id>(\x00{2}|\x22\x10))(?<nb_dev_id>.{2})(?<sb_vendor_id>(\x00{2}|\x22\x10))(?<sb_dev_id>..)(?<processor_sig>..)(?<nb_rev_id>.)(?<sb_rev_id>.)(?<bios_rev>[\x00\x01])(\x00{3}|\xAA{3})/mn
   bin_to_hex = lambda{|b| b.unpack("C*").map{|x|(x+256).to_s(16)[1,2]} }
 
+  file = File.open(path, mode:'rb')
+  raise 'wrong file format' if file.read(4) != "DMA\x00"
+  cpu_table_type = file.read(4).unpack1('I')
+  raise 'unkown cpu table type' if cpu_table_type != 0
+  header_size = file.read(4).unpack1('I') + 4*3
+
   File.read(path, mode:"rb").scan(regexp).map do |date, version, loaderid, size, _, checksum, nb_vendor_id, nb_dev_id, sb_vendor_id, sb_dev_id, processor_sig, nb_rev_id, sb_rev_id, bios_rev|
     year = bin_to_hex[date[0,2]].reverse.join
     {
@@ -24,5 +30,5 @@ def parse_amd_microcode_pkg(path)
 end
 
 if __FILE__ == $PROGRAM_NAME
-  p parse_amd_microcode_pkg(ARGV.first || "/lib/firmware/amd-ucode/microcode_amd.bin")
+  parse_amd_microcode_pkg(ARGV.first || "/lib/firmware/amd-ucode/microcode_amd.bin").each{|c| p c}
 end
