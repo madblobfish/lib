4373d52ca65e09c4e5a67eea345121ed01bbc342
- <->
2024-10-06 06:23:36 +0200

    ruby: stdlib: rrarray: simplify by removing nonprefill

--- ruby/stdlib/rrarray.rb
+++ ruby/stdlib/rrarray.rb
@@ -4,10 +4,9 @@ class RRArray
   alias :length :size
 
   def initialize(size)
-    @prefill = true
-    @size = size
     @index = 0
-    @arr = [nil] * (@prefill ? @size : 0)
+    @size = size
+    @arr = Array.new(size)
   end
 
   def inspect
@@ -22,38 +21,30 @@ class RRArray
   end
 
   def delete(elem)
-    if @prefill
-      return false if elem.nil?
-      changed = false
-      @arr.map! do |e|
-        if e == elem
-          changed = true
-          nil
-        else
-          e
-        end
+    return false if elem.nil?
+    changed = false
+    @arr.map! do |e|
+      if e == elem
+        changed = true
+        nil
+      else
+        e
       end
-      changed
-    else
-      @arr.delete(elem) ? true : false
     end
+    changed
   end
 
   def delete_last
     @index += @size -1
     @index %= @size
     elem = @arr[@index]
-    if @prefill
-      @arr[@index] = nil
-    else
-      @arr.delete_at(@index)
-    end
+    @arr[@index] = nil
     elem
   end
 
   def include?(elem)
     # with prefill we can not detect nil's
-    return false if @prefill && elem.nil?
+    return false if elem.nil?
     @arr.include?(elem)
   end
 end
@@ -67,10 +58,3 @@ raise 'aHHHHH' unless a.delete_last == nil && ! a.include?(nil)
 raise 'aHHHHHH' unless a.delete(4) == true && ! a.include?(4)
 raise 'aHHHHHHH' unless a.include?(2)
 raise 'aHHHHHHHH' unless a.size == 5 && a.length == 5
-# nonexistant non prefilled version
-begin
-  p = RRArray.new(5, true)
-  raise 'ah'
-rescue ArgumentError
-  # second optional param does not exist
-end
