a30a2862664f8d11d54b32b855dcb46b1784154b
- <->
2024-11-08 00:05:39 +0100

    ruby: stdlib: string: contains_japanese?: a function to detect if japanese only characters are contained in a string

---
+++ ruby/stdlib/string/contains_japanese?.rb
@@ -0,0 +1,7 @@
+class String
+  def contains_japanese?
+    # with kanji
+    # /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/.match?(self)
+    /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f]/.match?(self)
+  end
+end
